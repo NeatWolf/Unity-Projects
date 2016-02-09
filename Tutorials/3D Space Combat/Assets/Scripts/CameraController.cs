c2dd31b46c55b6dd4e4fe5f8d691f6f7a042ce99
markdion <mark.dion91@gmail.com>
2016-02-08 22:46:16 -0500

    First commit for 3D Space Shooter project
    Includes basic player and camera movement
    Random sphere spawning in the world for perspective
    Space skybox from Asset Store

---
+++ Tutorials/3D Space Combat/Assets/Scripts/CameraController.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraController : MonoBehaviour
+{
+    public float offset;
+    public Transform player;
+
+    public float moveSpeed;
+    public float rotationSpeed;
+
+    void LateUpdate()
+    {
+        Vector3 wantedPosition = player.position - (player.rotation * Vector3.forward) * offset;
+
+        transform.position = Vector3.Lerp(transform.position, wantedPosition, moveSpeed);
+        //transform.rotation = Quaternion.Lerp(transform.rotation, Quaternion.LookRotation(player.position - transform.position), rotationSpeed);
+        transform.rotation = Quaternion.Slerp(transform.rotation, player.rotation, rotationSpeed);
+    }
+}
---
+++ Tutorials/3D Space Combat/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour
+{
+    public float moveSpeed;
+    public float strafeSpeed;
+    public float lookSpeed;
+
+    private Rigidbody rb;
+    private Quaternion targetRotation;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    void FixedUpdate()
+    {
+        float inputVertical = Input.GetAxis("Vertical");
+        float inputHorizontal = Input.GetAxis("Horizontal");
+
+        // Look
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        Quaternion rayDirection = Quaternion.LookRotation(ray.direction);
+
+        transform.rotation = Quaternion.Slerp(transform.rotation, rayDirection, lookSpeed);
+
+        // Move
+        rb.velocity = (transform.rotation * Vector3.forward) * moveSpeed * inputVertical;
+        rb.velocity += (transform.rotation * Vector3.right) * strafeSpeed * inputHorizontal;
+    }
+}
