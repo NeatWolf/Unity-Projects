b19b5943c763c3e4a00eaf8c32efdf89010d1a39
markdion <mark.dion91@gmail.com>
2016-02-08 22:46:16 -0500

    First commit for 3D Space Shooter project
    Includes basic player and camera movement
    Random sphere spawning in the world for perspective
    Space skybox from Asset Store

---
+++ Tutorials/3D Space Combat/Assets/Scripts/GameController.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameController : MonoBehaviour {
+
+    public int hazardCount;
+    public GameObject[] hazards;
+    public Vector3 hazardSpawnPosition;
+
+	void Start ()
+    {
+        SpawnHazards();
+	}
+	
+    void SpawnHazards()
+    {
+        for (int i = 0; i < hazardCount; i++)
+        {
+            GameObject hazard = hazards[Random.Range(0, hazards.Length)];
+            Vector3 spawnPosition = new Vector3(Random.Range(-hazardSpawnPosition.x, hazardSpawnPosition.x), Random.Range(-hazardSpawnPosition.y, hazardSpawnPosition.y), Random.Range(-hazardSpawnPosition.z, hazardSpawnPosition.z));
+            Instantiate(hazard, spawnPosition, Quaternion.identity);
+        }
+    }
+}
