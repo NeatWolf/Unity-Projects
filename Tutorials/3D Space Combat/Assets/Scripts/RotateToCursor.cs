97420b1f47c01d8279b5fb48430c941f493bf1e7
markdion <mark.dion91@gmail.com>
2016-02-12 16:32:23 -0500

    Moved 3rd party libraries into a new folder, fixed aiming by adding raycasting that detects colliders in front of the cursor

--- Tutorials/3D Space Combat/Assets/Scripts/RotateToCursor.cs
+++ Tutorials/3D Space Combat/Assets/Scripts/RotateToCursor.cs
@@ -5,8 +5,19 @@ public class RotateToCursor : MonoBehaviour {
 
     public float distance;
 
-	void Update ()
+    private Ray ray;
+    private RaycastHit hit;
+
+    void Update ()
     {
+        // Adjust the distance to shoot towards based on the collider we're looking at
+        ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        if(Physics.Raycast(ray, out hit))
+        {
+            Debug.Log(hit.collider.name);
+            distance = hit.distance;
+        }
+
         // Get the direction the mouse is pointing
         Vector3 targetPosition = new Vector3(Input.mousePosition.x, Input.mousePosition.y, distance);
         targetPosition = Camera.main.ScreenToWorldPoint(targetPosition);
