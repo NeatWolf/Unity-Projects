44ea9923e9973ccae013836712eb71033bac517a
markdion <mark.dion91@gmail.com>
2016-02-12 00:41:17 -0500

    3D Space Combat: Destroy asteroids, HDR, warp speed
    - Added explosions on impact of primary weapon on asteroid
    - Added asteroid health and explosion on destroy
    - Added HDR for laser weapon
    - Added warp effect and super fast speed

---
+++ Tutorials/3D Space Combat/Assets/Scripts/Explode.cs
@@ -0,0 +1,49 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Explode : MonoBehaviour {
+
+    public GameObject destroyExplosion;
+    public GameObject impactExplosion;
+    public float health;
+
+    void Start() { }
+
+    //void OnTriggerEnter(Collider other)
+    //{
+    //    if (other.gameObject.CompareTag("Weapon"))
+    //    {
+    //        health--;
+    //        if (health == 0)
+    //        {
+    //            Instantiate(destroyExplosion, transform.position, transform.rotation);
+    //            Destroy(other.gameObject);
+    //            Destroy(gameObject);
+    //        }
+    //        else
+    //        {
+    //            Instantiate(impactExplosion, other.transform.position, other.transform.rotation);
+    //            Destroy(other.gameObject);
+    //        }
+    //    }
+    //}
+
+    void Update()
+    {
+        if(health <= 0)
+        {
+            Instantiate(destroyExplosion, transform.position, transform.rotation);
+            Destroy(gameObject);
+        }
+    }
+
+    void Damage(float damage)
+    {
+        health -= damage;
+    }
+
+    void ImpactExplosion(Vector3 position)
+    {
+        Instantiate(impactExplosion, position, transform.rotation);
+    }
+}
