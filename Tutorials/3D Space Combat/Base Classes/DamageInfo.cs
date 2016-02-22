3e43d9879e9b9b711ff85fbbf72e1cd311db6235
markdion <mark.dion91@gmail.com>
2016-02-22 01:30:34 -0500

    Created health bar panel to hold both empty and full images

---
+++ Tutorials/3D Space Combat/Base Classes/DamageInfo.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+
+public class DamageInfo
+{
+    private GameObject _sender;
+    private int _damage;
+
+    public GameObject Sender
+    {
+        get
+        {
+            return _sender;
+        }
+    }
+    public int Damage
+    {
+        get
+        {
+            return _damage;
+        }
+    }
+
+    public DamageInfo(GameObject sender, int damage)
+    {
+        _sender = sender;
+        _damage = damage;
+    }
+}
