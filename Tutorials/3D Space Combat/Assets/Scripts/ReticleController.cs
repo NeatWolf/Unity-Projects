5f3ea0b8a9e8acaa79af4f77d1089f3d732e301e
markdion <mark.dion91@gmail.com>
2016-03-22 00:15:16 -0400

    Added cursor for menus to replace reticle

--- Tutorials/3D Space Combat/Assets/Scripts/ReticleController.cs
+++ Tutorials/3D Space Combat/Assets/Scripts/ReticleController.cs
@@ -3,18 +3,39 @@ using System.Collections;
 
 public class ReticleController : MonoBehaviour {
 
+    public Texture2D reticleTexture;
     public Texture2D cursorTexture;
     public CursorMode cursorMode = CursorMode.Auto;
-    public Vector2 hotSpot = Vector2.zero;
+    public Vector2 reticleHotSpot = Vector2.zero;
+    public Vector2 cursorHotSpot = Vector2.zero;
+
+    private bool isPaused = false;
 
     void Start()
     {
-        Cursor.SetCursor(cursorTexture, hotSpot, cursorMode);
+        Cursor.SetCursor(reticleTexture, reticleHotSpot, cursorMode);
+    }
+
+    void Update()
+    {
+        bool gamePaused = GameManager.instance.isPaused;
+        if (gamePaused != isPaused)
+        {
+            isPaused = gamePaused;
+            if(gamePaused)
+            {
+                Cursor.SetCursor(cursorTexture, cursorHotSpot, cursorMode);
+            }
+            else
+            {
+                Cursor.SetCursor(reticleTexture, reticleHotSpot, cursorMode);
+            }
+        }
     }
 
     void OnMouseEnter()
     {
-        Cursor.SetCursor(cursorTexture, hotSpot, cursorMode);
+        Cursor.SetCursor(reticleTexture, reticleHotSpot, cursorMode);
     }
 
     void OnMouseExit()
