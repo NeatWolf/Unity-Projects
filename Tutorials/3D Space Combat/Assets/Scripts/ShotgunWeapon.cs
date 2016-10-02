b501851d799045f07efef1d7c7e08f68c584d707
markdion <mark.dion91@gmail.com>
2016-10-02 19:29:42 -0400

    Added initial version of shotgun
    Shotgun creates multiple bolts and shoots them in a cone away from the
    player ship

---
+++ Tutorials/3D Space Combat/Assets/Scripts/ShotgunWeapon.cs
@@ -0,0 +1,54 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ShotgunWeapon : MonoBehaviour {
+
+    public float fireRate;
+    public int boltsPerShot = 10;
+    public float bulletSpread = 15f;
+    public Transform shotSpawn;
+    public GameObject shotPrefab;
+    public AudioClip shootingClip;
+    public AudioSource[] shotAudioSources;
+    public float minShotVolume = 0.5f;
+    public float maxShotVolume = 1f;
+
+    private GameObject _currentShot;
+    private float _nextFire;
+
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+        if (GameManager.instance.isShootingEnabled && CompareTag("Player") && !GameManager.instance.isMenuOpen)
+        {
+            if (Input.GetMouseButtonDown(1) && Time.time > _nextFire)
+            {
+                _nextFire = Time.time + fireRate;
+                Fire();
+            }
+        }
+    }
+
+    private void PlayAudioSources()
+    {
+        foreach (var audioSource in shotAudioSources)
+        {
+            float volume = Random.Range(minShotVolume, maxShotVolume);
+            audioSource.PlayOneShot(shootingClip, volume);
+        }
+    }
+
+    private void Fire()
+    {
+        for (int i = 0; i < boltsPerShot; i++)
+        {
+            Vector3 rot = shotSpawn.rotation.eulerAngles;
+            Vector3 rotation = new Vector3(rot.x + Random.Range(-bulletSpread, bulletSpread), rot.y + Random.Range(-bulletSpread, bulletSpread), rot.z + Random.Range(-bulletSpread, bulletSpread));
+            Instantiate(shotPrefab, shotSpawn.position, Quaternion.Euler(rotation));
+        }
+    }
+}
